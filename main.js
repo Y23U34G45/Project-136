065314480b776eeb9bfaf9e15c91a0e3bbb30da7
Y23U34G45 <99113923+Y23U34G45@users.noreply.github.com>
2022-05-17 14:47:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+Status = "";
+input_text = "";
+
+function setup(){
+canvas = createCanvas(480, 380);
+canvas.center();
+video = createCapture(VIDEO);
+vidoe.size(300, 290);
+video.hide();
+}
+function start(){
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+    input_text= document.getElementById("input_id").value;
+}
+function modelLoaded(){
+    console.log("model_Loaded");
+    Status = true;
+}
+function draw(){
+    image(video,0,0,300,290);
+}
